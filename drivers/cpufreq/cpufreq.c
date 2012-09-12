c3a4d4a0c7f2bf0a1eca3a5f108d8b46c65276fb
吴文彬 <wenbinwu@meizu.com>
2012-09-12 06:55:41 +0000

    1.cpufreq: 修复错误的最大频率显示

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -373,7 +373,7 @@ static ssize_t show_cpuinfo_max_freq(struct cpufreq_policy *policy, char *buf)
 	unsigned int freq = 1400000;
 	if (machine_is_m040())
 		freq = 1600000;	
-	if (machine_is_m030() || machine_is_m032())
+	else if (machine_is_m030() || machine_is_m032())
 		freq = 1400000;
 	else if (machine_is_m031())
 		freq = 1500000;
