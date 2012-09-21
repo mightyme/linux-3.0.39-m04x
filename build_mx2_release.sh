1affe4bb4ecd66b01714d011b92883c1bd25f052
郑康亮 <zhengkl@meizu.com>
2012-09-21 07:19:38 +0000

    修改 build_mx2_release.sh build_release.sh脚本:增加保存vmlinux文件功能

--- build_mx2_release.sh
+++ build_mx2_release.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
-# Some tools requires an english envrionment
-export LANG="en_US.UTF-8"
-export LANGUAGE="en_US:en"
-export CROSS_COMPILE=arm-none-linux-gnueabi-
-CPU_JOB_NUM=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')
+function get_svn_ver()
+{
+	if [ -d .svn ];then
+		if rev=`svn info 2>/dev/null` ; then
+			if [ $(locale |grep LANGUAGE|grep zh_CN) ];then
+				rev=`echo "${rev}" | grep '^最后修改的版本' | awk '{print $NF}'`
+			else
+				rev=`echo "${rev}" | grep '^Last Changed Rev' | awk '{print $NF}'`
+			fi
+			printf -- '%s' $rev
+		fi 
+	else
+		printf '0' 
+	fi
+}
 
-svn up
+function save_image()
+{
+	local config=$(echo ${1#*_})
 
-make distclean -j$CPU_JOB_NUM
+	config=$(echo ${config%_*})
+	cp vmlinux r$1.$config.vmlinux
+	cp System.map r$1.$config.System.map
+}
 
-mkdir -p output
+function save_all_image()
+{
+	if [ -d ~/dev/kernelimage ];then
+		cp output ~/dev/kernelimage/r$1 -r
+	fi
+}
 
 function gen_all_kernel_image()
 {
+	# Some tools requires an english envrionment
+	export LANG="en_US.UTF-8"
+	export LANGUAGE="en_US:en"
+	export CROSS_COMPILE=arm-none-linux-gnueabi-
+	CPU_JOB_NUM=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')
+
+	mkdir -p output
+	svn up
+	make distclean -j$CPU_JOB_NUM
+
+	local svn_rev=$(get_svn_ver)
+
 	function check_kernel_build_result() {
 		if [ $? != 0 ];then 
 			echo "$1 build error!!"
@@ -25,27 +57,36 @@ function gen_all_kernel_image()
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx2_recovery_defconfig
 	mv arch/arm/boot/zImage output/zImage-recovery
+	save_image mx2_recovery_defconfig
 
 	make mx2_user_defconfig 
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx2_user_defconfig 
 	mv arch/arm/boot/zImage output/zImage
+	save_image mx2_user_defconfig
 
 	make mx2_overseas_defconfig
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx2_overseas_defconfig
 	mv arch/arm/boot/zImage output/zImage-overseas
+	save_image mx2_overseas_defconfig
 
 	make mx2_eng_defconfig
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx2_eng_defconfig
 	mv arch/arm/boot/zImage output/zImage-dev
-	
-	unset check_kernel_build_result
+	save_image mx2_eng_defconfig
 
+	save_all_image $svn_rev
+
+	unset check_kernel_build_result
 }
 
-gen_all_kernel_image
-
-unset gen_all_kernel_image
+if [ $# -gt 0 -a "x$1" == "xmain" ];then
+	gen_all_kernel_image
+	unset gen_all_kernel_image
+	unset get_svn_ver
+	unset save_image
+	unset save_all_image
+fi
 
--- build_release.sh
+++ build_release.sh
@@ -1,56 +1,97 @@
 #!/bin/bash
 
-# Some tools requires an english envrionment
-export LANG="en_US.UTF-8"
-export LANGUAGE="en_US:en"
+function get_svn_ver()
+{
+	if [ -d .svn ];then
+		if rev=`svn info 2>/dev/null` ; then
+			if [ $(locale |grep LANGUAGE|grep zh_CN) ];then
+				rev=`echo "${rev}" | grep '^最后修改的版本' | awk '{print $NF}'`
+			else
+				rev=`echo "${rev}" | grep '^Last Changed Rev' | awk '{print $NF}'`
+			fi
+			printf -- '%s' $rev
+		fi 
+	else
+		printf '0' 
+	fi
+}
 
-CPU_JOB_NUM=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')
+function save_image()
+{
+	local config=$(echo ${1#*_})
 
-svn up
+	config=$(echo ${config%_*})
+	cp vmlinux r$1.$config.vmlinux
+	cp System.map r$1.$config.System.map
+}
 
-make distclean -j$CPU_JOB_NUM
-
-mkdir -p output
+function save_all_image()
+{
+	if [ -d ~/dev/kernelimage ];then
+		cp output ~/dev/kernelimage/r$1 -r
+	fi
+}
 
 function gen_all_kernel_image()
 {
+	# Some tools requires an english envrionment
+	export LANG="en_US.UTF-8"
+	export LANGUAGE="en_US:en"
+	CPU_JOB_NUM=$(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')
+
+	mkdir -p output
+	svn up
+	make distclean -j$CPU_JOB_NUM
+
+	local svn_rev=$(get_svn_ver)
+
 	function check_kernel_build_result() {
-		if [ $? != 0 ];then 
+		if [ $? != 0 ];then
 			echo "$1 build error!!"
 			exit -1
 		fi
 	}
 
-	make mx_user_defconfig 
+	make mx_user_defconfig
 	make all -j$CPU_JOB_NUM
-	check_kernel_build_result mx_user_defconfig 
+	check_kernel_build_result mx_user_defconfig
 	mv arch/arm/boot/zImage output/zImage
+	save_image mx_user_defconfig
 
 	make mx_overseas_defconfig
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx_overseas_defconfig
 	mv arch/arm/boot/zImage output/zImage-overseas
+	save_image mx_overseas_defconfig
 
 	make mx_release_recovery_defconfig
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx_release_recovery_defconfig
 	mv arch/arm/boot/zImage output/zImage-release-recovery
+	save_image mx_release_recovery_defconfig
 
 	make mx_recovery_defconfig
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx_recovery_defconfig
 	mv arch/arm/boot/zImage output/zImage-recovery
+	save_image mx_recovery_defconfig
 
 	make mx_eng_defconfig
 	make all -j$CPU_JOB_NUM
 	check_kernel_build_result mx_eng_defconfig
 	mv arch/arm/boot/zImage output/zImage-dev
-	
-	unset check_kernel_build_result
+	save_image mx_eng_defconfig
 
+	save_all_image $svn_rev
+
+	unset check_kernel_build_result
 }
 
-gen_all_kernel_image
-
-unset gen_all_kernel_image
+if [ $# -gt 0 -a "x$1" == "xmain" ];then
+	gen_all_kernel_image
+	unset gen_all_kernel_image
+	unset get_svn_ver
+	unset save_image
+	unset save_all_image
+fi
 
