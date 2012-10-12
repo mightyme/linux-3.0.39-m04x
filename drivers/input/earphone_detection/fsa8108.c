54928b23ac456e1a10dbf5a164ef28ab832751ad
罗楚成 <luochucheng@meizu.com>
2012-10-12 07:57:47 +0000

    有些手机检测IC复位不正常，添加软件复位和调试接口

--- drivers/input/earphone_detection/fsa8108.c
+++ drivers/input/earphone_detection/fsa8108.c
@@ -196,7 +196,9 @@
 #define NO_SE_110     0x09
 #define NO_SE_120     0x0a  //default
 #define NO_SE_130     0x0b
+#define NO_SE_170     0x0f
 //FSA8108_NC_SE_KEY_CMP [7:4]
+#define NC_SE_1000    0x00
 #define NC_SE_2200    0x0d
 #define NC_SE_2300    0x0e  //default
 #define NC_SE_2400    0x0f
@@ -279,14 +281,130 @@ static BYTE fsa8108_GetValue(BYTE reg, BYTE reg_bit, BYTE reg_shift)
 
 	return ret;
 }
+
+static ssize_t fsa8108_show(struct device *dev,
+                                      struct device_attribute *attr,
+                                      char *buf);
+static ssize_t fsa8108_store(struct device *dev, 
+			     struct device_attribute *attr,
+			     const char *buf, size_t count);
+
+#define FSA_ATTR(_name)\
+{\
+    .attr = { .name = #_name, .mode = S_IRUGO | S_IWUSR},\
+    .show = fsa8108_show,\
+    .store = fsa8108_store,\
+}
+
+static struct device_attribute fsa8108_attrs[] = {
+    FSA_ATTR(RegW),
+    FSA_ATTR(RegR),
+};
+enum {
+	REG_SETVALUE,
+	REG_GETVALUE,
+};
+
+static ssize_t fsa8108_show(struct device *dev,
+                                      struct device_attribute *attr,
+                                      char *buf)
+{
+	int i = 0;
+	ptrdiff_t off;
+	off = attr - fsa8108_attrs;
+
+	switch(off){
+	case REG_SETVALUE:
+		i += scnprintf(buf+i, PAGE_SIZE-i, "Error\n");
+		break;
+	case REG_GETVALUE:
+		{
+			int reg,value;
+			for(reg = FSA8108_REG_DEVICE_ID;reg <= FSA8108_REG_RESET;reg++)
+			{
+				value = fsa8108_ReadReg(reg);
+				pr_info("%s: R=0x%.2X D=0x%.2X \n", __func__, reg,value);
+			}
+		}
+		i += scnprintf(buf+i, PAGE_SIZE-i, "0x%.2X 0x%.2X\n",fsa8108_ReadReg(2),fsa8108_ReadReg(3));
+		break;
+	default:
+		i += scnprintf(buf+i, PAGE_SIZE-i, "Error\n");
+		break;	
+	}
+	return i;
+}
+
+static ssize_t fsa8108_store(struct device *dev, 
+			     struct device_attribute *attr,
+			     const char *buf, size_t count)
+{
+	unsigned int reg,value;
+	int ret = 0;
+	ptrdiff_t off;
+
+	off = attr - fsa8108_attrs;
+
+	switch(off){
+	case REG_SETVALUE:
+		if (sscanf(buf, "%X %X \n", &reg,&value) == 2) {	
+			fsa8108_WriteReg(reg,value);
+			pr_info("%s: R=0x%.2X D=0x%.2X \n", __func__, reg,value);
+		}
+		ret = count;
+		break;
+	case REG_GETVALUE:
+		if (sscanf(buf, "%X\n", &reg) == 1) {	
+			
+			value = fsa8108_ReadReg(reg);
+			pr_info("%s: R=0x%.2X D=0x%.2X \n", __func__, reg,value);
+			
+		}
+		ret = count;
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+	return ret;	
+}
+
+static int fsa8108_create_attrs(struct device * dev)
+{
+	int i, rc;
+
+	for (i = 0; i < ARRAY_SIZE(fsa8108_attrs); i++) {
+		rc = device_create_file(dev, &fsa8108_attrs[i]);
+		if (rc)
+			goto fsa8108_attrs_failed;
+	}
+	goto succeed;
+
+fsa8108_attrs_failed:
+	printk(KERN_INFO "%s(): failed!!!\n", __func__);	
+	while (i--)
+		device_remove_file(dev, &fsa8108_attrs[i]);
+succeed:		
+	return rc;
+
+}
+
+static void fsa8108_destroy_atts(struct device * dev)
+{
+	int i;
+	for (i = 0; i < ARRAY_SIZE(fsa8108_attrs); i++)
+		device_remove_file(dev, &fsa8108_attrs[i]);
+}
+
+
 static void fsa8108_mask_int(int onoff)
 {
 	if(onoff){
-		fsa8108_WriteReg(FSA8108_REG_INT_MASK_1,0X3B);
-		fsa8108_WriteReg(FSA8108_REG_INT_MASK_2,0X3F);
+		fsa8108_WriteReg(FSA8108_REG_INT_MASK_1,0xFB);
+		fsa8108_WriteReg(FSA8108_REG_INT_MASK_2,0xFF);
 	}else{
-		fsa8108_WriteReg(FSA8108_REG_INT_MASK_1,0);
-		fsa8108_WriteReg(FSA8108_REG_INT_MASK_2,0);
+		fsa8108_WriteReg(FSA8108_REG_INT_MASK_1,0xC0);
+		fsa8108_WriteReg(FSA8108_REG_INT_MASK_2,0xC0);
 	}
 }
 
@@ -321,7 +439,7 @@ EXPORT_SYMBOL(fsa8108_MP3_mode);
 * Function: fsa8108_LDO_output(int onoff)
 *
 * Parameters: onoff - LDO out ON : 1
-*                               LDO out OFF : 0
+*                             LDO out OFF : 0
 *             
 * Return: None
 *
@@ -343,7 +461,7 @@ static void process_int(int intr_type,struct fsa8108_info* info)
 	unsigned char val1, val2;
 	val1 = intr_type & 0xff;
 	val2 = intr_type >> 8;	
-	pr_info("\nvalue = 0x%x,val1 = 0x%x,val2 = 0x%x\n",intr_type, val1, val2);
+	pr_info("\nvalue = 0x%.4X,val1 = 0x%x,val2 = 0x%x\n",intr_type, val1, val2);
 
 	if(val1)
 	{
@@ -468,7 +586,7 @@ static void process_int(int intr_type,struct fsa8108_info* info)
 	}
 	else
 	{
-	    pr_err("%s : interrupt trigger is wrong.\n", __func__);
+	    pr_err("%s : interrupt trigger is wrong. intr_type = 0x%.4X \n", __func__,intr_type);
 	}
 
 }
@@ -491,62 +609,72 @@ static void fsa8081_jack_det_work_func(struct work_struct *work)
 	struct i2c_client *client = info->client;
 	int intr_type;	
 
-    mutex_lock(&info->mutex);
+	mutex_lock(&info->mutex);
 	intr_type = i2c_smbus_read_word_data(client, FSA8108_REG_INT_1);
 	process_int(intr_type,info);
 	mutex_unlock(&info->mutex);
-
 }
 
 //extern void set_ext_mic_bias(bool bOnOff);
 //extern bool get_ext_mic_bias(void);
 static void fsa8108_initialization(struct fsa8108_info *info)
 {
-    struct i2c_client *client = info->client;
+	struct i2c_client *client = info->client;
 	int int_type=0;	
-/*** Set Comparator Thresholds setting Send/End***/
-    fsa8108_SetValue(FSA8108_REG_COMPARATOR_12, FSA8108_NO_SE_KEY_CMP,
-    FSA8108_NO_SE_KEY_CMP_SHIFT, 0x0F);//0.083 //0.05
-/*** Set Comparator Thresholds setting for volum up***/
-//	fsa8108_SetValue(FSA8108_REG_COMPARATOR_34, FSA8108_VOL_UP_CMP, 
-//	FSA8108_VOL_UP_CMP_SHIFT , /*0x0d*/0x06);//0.299 //0.18 //default 0.25
-/*** Set Comparator Thresholds setting for volum down***/
+	
+	/*** Set Comparator Thresholds setting Send/End***/
+	fsa8108_SetValue(FSA8108_REG_COMPARATOR_12, FSA8108_NO_SE_KEY_CMP,
+	FSA8108_NO_SE_KEY_CMP_SHIFT, NO_SE_170);//0.083 //0.05
+	fsa8108_SetValue(FSA8108_REG_COMPARATOR_12, FSA8108_NC_SE_KEY_CMP,
+	FSA8108_NC_SE_KEY_CMP_SHIFT, NC_SE_2300);//2300mV
+    
+	/*** Set Comparator Thresholds setting for volum up***/
+	//	fsa8108_SetValue(FSA8108_REG_COMPARATOR_34, FSA8108_VOL_UP_CMP, 
+	//	FSA8108_VOL_UP_CMP_SHIFT , /*0x0d*/0x06);//0.299 //0.18 //default 0.25
+
+	
+	/*** Set Comparator Thresholds setting for volum down***/
 	fsa8108_SetValue(FSA8108_REG_COMPARATOR_34, FSA8108_VOL_DOWN_CMP, 
 	FSA8108_VOL_DOWN__CMP_SHIFT , /*0x06*/0x0F);//0.620 //0.29
 
-/*** Set Timing parameters and Global Multiplier setting ***/
-    fsa8108_SetValue(FSA8108_REG_KEY_PRS_T,FSA8108_TDOUBLE,FSA8108_TDOUBLE_SHIFT,0x02);
 
-/*** Set Control bits: All key/Double/Long/3-4 pole/LDO ***/
+	/*** Set Timing parameters and Global Multiplier setting ***/
+	fsa8108_SetValue(FSA8108_REG_KEY_PRS_T,FSA8108_TDOUBLE,FSA8108_TDOUBLE_SHIFT,0x02);
+
+	/*** Set Control bits: All key/Double/Long/3-4 pole/LDO ***/
 
 	//set_ext_mic_bias(1); internal micbiasd provided,this sentence unnecessary
 	//clear interrupts
+	
+	fsa8108_mask_int(0);
+
 	int_type = i2c_smbus_read_word_data(client, FSA8108_REG_INT_1);
 	process_int(int_type,info); 
 
 }
+
+
 static void power_on(int onoff)
 {
 	return;
 }
+
 static int fsa8108_probe(
 	struct i2c_client *client, const struct i2c_device_id *id)
 {
 
 	int i,ret = 0;
 	struct fsa8108_info *info;
-//	#define KEY_FN_F1		0x1d2
-//	#define KEY_FN_F2		0x1d3
-//	#define KEY_FN_F3		0x1d4
-//	#define KEY_FN_F4		0x1d5
-//	#define KEY_FN_F5		0x1d6
-//	#define KEY_FN_F6		0x1d7
+
 	int fsa8108_jack_keycode[] = {KEY_MEDIA, KEY_VOLUMEUP, KEY_VOLUMEDOWN
 								,KEY_FN_F1, KEY_FN_F2, KEY_FN_F3
 								,KEY_FN_F4, KEY_FN_F5, KEY_FN_F6};
 	int device_id = -1;
+	
+	//power_on(1);
+	//msleep(100);
 
-	printk("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$earphone detection fsa8108_probe begin\n");	
+	printk("[fsa8108]earphone detection fsa8108_probe begin\n");	
 	info = kzalloc(sizeof(struct fsa8108_info), GFP_KERNEL);
 	if(info == NULL){
 		pr_info("%s  allocate memory for info failed\n",__func__);
@@ -557,6 +685,13 @@ static int fsa8108_probe(
 	mutex_init(&info->mutex);
 
 	this_client = client;
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE_DATA)) {
+		pr_err("%s: i2c check functionality error\n", __func__);
+		ret = -ENODEV;
+		goto check_funcionality_failed;
+	}
+
+	//pr_info("%s: FSA8108_REG_COMPARATOR_12=0x%.2X \n", __func__,fsa8108_ReadReg(FSA8108_REG_COMPARATOR_12));
 
 	device_id = fsa8108_ReadReg(FSA8108_REG_DEVICE_ID);
 	pr_info("%s: FSADEVICE_ID =%d \n",__func__,device_id);
@@ -565,11 +700,11 @@ static int fsa8108_probe(
 		ret = -ENODEV;
 		goto check_device_failed;
 	}	
-	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE_DATA)) {
-		pr_err("%s: i2c check functionality error\n", __func__);
-		ret = -ENODEV;
-		goto check_funcionality_failed;
-	}
+
+	// Reset
+	fsa8108_WriteReg(FSA8108_REG_RESET,0x01);
+	msleep(100);
+	fsa8108_WriteReg(FSA8108_REG_RESET,0x00);
 
 	info->input = input_allocate_device();
 	if (info->input == NULL) {
@@ -614,15 +749,17 @@ static int fsa8108_probe(
 		    dev_err(&client->dev, "failed to reqeust IRQ\n");
 		    return ret;
 	    }
-		/*pr_info("fsa8081 irq:%d",client->irq);206*/
-	    ret = enable_irq_wake(client->irq);
-	    if (ret < 0)
-		    dev_err(&client->dev,
-			    "failed to enable wakeup src %d\n", ret);
     }
-	power_on(1);
-	msleep(100);
 	fsa8108_initialization(info);
+	
+	/*pr_info("fsa8081 irq:%d",client->irq);206*/
+	ret = enable_irq_wake(client->irq);
+	if (ret < 0)
+		dev_err(&client->dev,
+			"failed to enable wakeup src %d\n", ret);
+
+	fsa8108_create_attrs(&client->dev);
+	
 	return ret;
 
 err_switch_dev_register:
@@ -644,6 +781,7 @@ static int fsa8108_remove(struct i2c_client *client)
 {
     struct fsa8108_info *info = i2c_get_clientdata(client);
 
+	fsa8108_destroy_atts(&client->dev);
 	power_on(0);
 
 	if (client->irq) {
@@ -699,7 +837,3 @@ static __exit void fsa8108_i2c_exit(void)
 
 module_init(fsa8108_i2c_init);
 module_exit(fsa8108_i2c_exit);
-
-MODULE_AUTHOR("Chris Jeong <chris.jeong@fairchildsemi.com");
-MODULE_DESCRIPTION("I2C bus driver for FSA8108 Jack detection IC");
-MODULE_LICENSE("GPL v2");
