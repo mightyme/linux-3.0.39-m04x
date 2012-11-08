815b8bac45dedf1e96a657003894364a8c7308a6
吴文彬 <wenbinwu@meizu.com>
2012-11-08 02:28:43 +0000

    Touchscreen: 1.改善唤醒系统后，触摸屏无反应问题；2.添加优化USB设备干扰处理
    merge from:
    svn://172.16.9.63/svn_src/kernel/meizu_m040/linux-3.0.39-rtm-mp-dev@9206

--- include/linux/rmi.h
+++ include/linux/rmi.h
@@ -145,6 +145,7 @@ struct rmi_device_platform_data {
 	char *sensor_name;	/* Used for diagnostics. */
 
 	int attn_gpio;
+	int vbus_gpio;
 	enum rmi_attn_polarity attn_polarity;
 	bool level_triggered;
 	void *gpio_data;
