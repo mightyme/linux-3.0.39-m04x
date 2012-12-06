87ff3ec1379bad5311138827cc4fbbeb1dfbe1be
吴文彬 <wenbinwu@meizu.com>
2012-12-06 09:32:56 +0000

    voip: fix latency and mapping
    svn://172.16.9.63/svn_src/kernel/meizu_m040/linux-3.0.39-rtm-mp-dev@9550

--- sound/soc/codecs/wm8958_path.h
+++ sound/soc/codecs/wm8958_path.h
@@ -53,12 +53,12 @@
 #define	VOL_INCALL_HP				(0x39+2)							// 4dB
 #define	VOL_INCALL_HP2				(0<<WM8994_HPOUT2_VOL_SHIFT)		// 0:0dB  1: -6dB
 
-#define	VOL_VOIP_SPK				(0x39+4)							//4dB 0dB = 0x39
-#define	VOL_VOIP_SPK_BOOST			(4<<WM8994_SPKOUTL_BOOST_SHIFT)		// 6dB
+#define	VOL_VOIP_SPK_BOOST			(7<<WM8994_SPKOUTL_BOOST_SHIFT)		// 6dB
+#define	VOL_VOIP_SPK				(0x39+3)							//4dB 0dB = 0x39
 #define	VOL_VOIP_REC				(0x39+6)							// 6dB
+#define	VOL_VOIP_AIF2DAC_BOOST		(0<<WM8994_AIF2DAC_BOOST_SHIFT)		// 6dB
+#define	VOL_VOIP_HP					(0x39+2)							// 0dB
 #define	VOL_VOIP_HP2				(0<<WM8994_HPOUT2_VOL_SHIFT)		// 0:0dB  1: -6dB
-#define	VOL_VOIP_HP					(0x39+0)							// 0dB
-#define	VOL_VOIP_AIF2DAC_BOOST			(1<<WM8994_AIF2DAC_BOOST_SHIFT)		// 6dB
 
 // 0x19 = +21dB  0x1F = +30dB 0x07 = -6dB
 #define	VOL_IN1L_BOOST_SPK					(1<<WM8994_IN1L_MIXINL_VOL_SHIFT)	// +30dB
