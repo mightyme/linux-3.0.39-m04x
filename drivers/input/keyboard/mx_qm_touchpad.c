53c2f84194c5e1aea2986332c99b99a4e715f2dc
吴文彬 <wenbinwu@meizu.com>
2012-12-14 09:04:53 +0000

    Touchpad: 添加双击按键支持接口
    svn://172.16.223.26/svn_src/kernel/meizu_m040/linux-3.0.39-rtm-mp-dev@9567

--- drivers/input/keyboard/mx_qm_touchpad.c
+++ drivers/input/keyboard/mx_qm_touchpad.c
@@ -29,6 +29,10 @@
 #include <linux/firmware.h>
 #include	<linux/mx_qm.h>
 
+#define	HAVE_DOUBLE_KEY
+#define	DOUBLE_CHECK_TIME		200	//ms
+#define	KEY_DOUBLE				KEY_AGAIN
+
 struct mx_qm_touch {
 	 struct mx_qm_data *data;
 	struct input_dev *input_key;
@@ -38,7 +42,52 @@ struct mx_qm_touch {
 	 struct early_suspend early_suspend;
 	 int early_suspend_flag;
 #endif
+#ifdef HAVE_DOUBLE_KEY
+	struct delayed_work doublekey_ckeck_func;
+	u8 key_press_cunt;
+	u8 key_need_release_flag;
+	ktime_t last_key_release_time;
+	ktime_t last_key_press_time;
+#endif	
 };
+
+#ifdef HAVE_DOUBLE_KEY
+int issuport_doublekey = 0;
+static ssize_t qm_touch_keytype_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t count)
+{
+	 int value;
+	 
+	if (sscanf(buf, "%d ", &value) != 1)
+		return -EINVAL;
+	
+	issuport_doublekey = !!value;
+	
+	return count;
+}
+
+static ssize_t qm_touch_keytype_show(struct device *dev,
+				   struct device_attribute *attr,
+				   char *buf)
+{
+	
+	return sprintf(buf,"%d\n", issuport_doublekey);
+}
+
+
+static DEVICE_ATTR(doublekey, 0666, qm_touch_keytype_show, qm_touch_keytype_store);
+
+static struct attribute *touchpad_attrs[] = {
+	&dev_attr_doublekey.attr,
+	NULL
+};
+
+static const struct attribute_group touchpad_attr_group = {
+	.attrs = touchpad_attrs,
+};
+#endif	
+
 void qm_touch_report_key(struct input_dev *dev, unsigned int code, int value)
 {
 	pr_debug("%s:KeyCode = %d  S = %d  \n",__func__,code,value);
@@ -46,11 +95,43 @@ void qm_touch_report_key(struct input_dev *dev, unsigned int code, int value)
 	input_sync(dev);
 }
 
+#ifdef HAVE_DOUBLE_KEY
+static void qm_touch_doublekey_ckeck_work_func(struct work_struct *work)
+{
+	struct mx_qm_touch *touch = 
+		container_of(work, struct mx_qm_touch, doublekey_ckeck_func.work);
+	struct input_dev *input = touch->input_key;
+
+	if( touch->key_press_cunt > 1 )
+	{
+		qm_touch_report_key( input,KEY_DOUBLE,1 );	
+		qm_touch_report_key( input,KEY_DOUBLE,0 );	
+	}	
+	else if( touch->key_press_cunt == 1 )
+	{
+		if( touch->keys_press  )
+		{
+			qm_touch_report_key( input,KEY_HOME,1 );	
+			touch->key_need_release_flag = true;
+		}
+		else
+		{
+			qm_touch_report_key( input,KEY_HOME,1 );	
+			qm_touch_report_key( input,KEY_HOME,0 );	
+		}
+	}
+
+	touch->key_press_cunt = 0;
+		
+}
+#endif
+
 static irqreturn_t mx_qm_irq_handler(int irq, void *dev_id)
 {
 	struct mx_qm_touch *touch = dev_id;
 	struct mx_qm_data *qm = touch->data;
 	struct input_dev *input = touch->input_key;
+	ktime_t time;
 	u8 bpress;
 
 	bpress = !(gpio_get_value(qm->gpio_irq));
@@ -64,8 +145,43 @@ static irqreturn_t mx_qm_irq_handler(int irq, void *dev_id)
 	{
 		if( touch->keys_press != bpress)
 		{
-			qm_touch_report_key( input,KEY_HOME,bpress );	
 			touch->keys_press = bpress;
+			
+		#ifndef HAVE_DOUBLE_KEY
+			qm_touch_report_key( input,KEY_HOME,bpress );	
+		#else
+			if(issuport_doublekey)
+			{
+				time = ktime_get();
+				if( bpress )
+				{
+					touch->last_key_press_time = time;
+					
+					if(touch->key_press_cunt == 0)
+						schedule_delayed_work(&touch->doublekey_ckeck_func, msecs_to_jiffies(DOUBLE_CHECK_TIME));
+					
+					touch->key_press_cunt ++;
+
+					pr_debug("%s:Key release %llu ms.\n",__func__,ktime_to_ms(ktime_sub(time,touch->last_key_release_time)));	
+				}
+				else
+				{
+					touch->last_key_release_time = time;
+					if(touch->key_need_release_flag)
+					{
+						qm_touch_report_key( input,KEY_HOME,0 );	
+						touch->key_need_release_flag = false;
+					}
+						
+					pr_debug("%s:Key pressed %llu ms.\n",__func__,ktime_to_ms(ktime_sub(time,touch->last_key_press_time)));					
+				}	
+			}
+			else
+			{
+				qm_touch_report_key( input,KEY_HOME,bpress );	
+			}
+		#endif				
+				
 		}
 	}
 	
@@ -80,9 +196,26 @@ static irqreturn_t mx_qm_irq_handler(int irq, void *dev_id)
 	 struct mx_qm_touch *touch =
 			 container_of(h, struct mx_qm_touch, early_suspend);
 	struct mx_qm_data * mx = touch->data;
+	struct input_dev *input = touch->input_key;
+	
+	//set_irq_flags(touch->irq,IRQF_TRIGGER_LOW);
 
-	touch->early_suspend_flag = true;
+	touch->early_suspend_flag = true;	
 	mx->i2c_writebyte(mx->client, QM_REG_STATUS,QM_STATE_SLEEP);
+
+#ifdef HAVE_DOUBLE_KEY
+	if( touch->key_press_cunt > 0)
+	{
+		cancel_delayed_work_sync(&touch->doublekey_ckeck_func);
+		if(touch->key_need_release_flag)
+		{
+			qm_touch_report_key( input,KEY_HOME,0 );	
+			touch->key_need_release_flag = false;
+		}
+
+		touch->key_press_cunt = 0;			
+	}
+#endif
 }
  
  static void mx_qm_touch_late_resume(struct early_suspend *h)
@@ -93,7 +226,8 @@ static irqreturn_t mx_qm_irq_handler(int irq, void *dev_id)
 
 	mx->i2c_writebyte(mx->client, QM_REG_STATUS,QM_STATE_NORMAL);
 	touch->early_suspend_flag = false;
- }
+	//set_irq_flags(touch->irq,IRQF_TRIGGER_FALLING|IRQF_TRIGGER_RISING); 
+}		
 #endif 
 
 static int __devinit mx_qm_touch_probe(struct platform_device *pdev)
@@ -135,6 +269,9 @@ static int __devinit mx_qm_touch_probe(struct platform_device *pdev)
 	
 	 __set_bit(EV_KEY, input_key->evbit);
 	 __set_bit(KEY_HOME, input_key->keybit);
+#ifdef HAVE_DOUBLE_KEY
+	 __set_bit(KEY_DOUBLE, input_key->keybit);
+#endif
 	input_set_drvdata(input_key, data);
 	
 	/* Register the input_key device */
@@ -161,6 +298,14 @@ static int __devinit mx_qm_touch_probe(struct platform_device *pdev)
 	 touch->early_suspend.resume = mx_qm_touch_late_resume;
 	 register_early_suspend(&touch->early_suspend);
 #endif
+	 
+#ifdef HAVE_DOUBLE_KEY
+	INIT_DELAYED_WORK(&touch->doublekey_ckeck_func, qm_touch_doublekey_ckeck_work_func);
+
+	err = sysfs_create_group(&data->dev->kobj, &touchpad_attr_group);
+	if(err < 0)
+		 dev_err(&client->dev, "fail to sysfs create group touchpad_attrs\n");
+#endif
 
 	 pr_debug("%s:--\n",__func__);
 	 return 0;
@@ -179,7 +324,12 @@ static int __devinit mx_qm_touch_probe(struct platform_device *pdev)
 static int __devexit mx_qm_touch_remove(struct platform_device *pdev)
 {
 	struct mx_qm_touch * touch = platform_get_drvdata(pdev);
-//	struct mx_qm_data * mx = touch->data;
+	struct mx_qm_data * mx = touch->data;
+
+#ifdef HAVE_DOUBLE_KEY
+	sysfs_remove_group(&mx->dev->kobj, &touchpad_attr_group);
+	cancel_delayed_work_sync(&touch->doublekey_ckeck_func);
+#endif
 	
 #ifdef CONFIG_HAS_EARLYSUSPEND
 	 unregister_early_suspend(&touch->early_suspend);
